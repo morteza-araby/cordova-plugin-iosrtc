7f569dd5887bc0285374754d5ccda44d1f3d2bae
Morteza Araby <morteza.araby@ericsson.com>
2017-04-06 12:41:47 +0200

    Added webrtcDetectDCSupport which Adabper.js need

--- dist/cordova-plugin-iosrtc.js
+++ dist/cordova-plugin-iosrtc.js
@@ -2534,6 +2534,7 @@ function registerGlobals() {
 	window.MediaStream                      = MediaStream;
 	window.webkitMediaStream                = MediaStream;
 	window.MediaStreamTrack                 = MediaStreamTrack;
+	window.webrtcDetectedDCSupport			= "RTP";
 }
 
 
